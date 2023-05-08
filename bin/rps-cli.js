849becb97a4599c8e0795720c2524afd4b87ae5f
SwaggyLTW <tommy22@live.unc.edu>
2023-05-08 18:22:37 -0400

    different function import statements

--- bin/rps-cli.js
+++ bin/rps-cli.js
@@ -1,5 +1,5 @@
 #!/usr/bin/env node
-
+import { rps } from 'node-rpsls'
 import minimist from "minimist";
 
 const args = minimist(process.argv.slice(2));
--- bin/rpsls-cli.js
+++ bin/rpsls-cli.js
@@ -1,5 +1,5 @@
 #!/usr/bin/env node
-
+import { rpsls } from 'node-rpsls'
 import minimist from "minimist";
 
 const args = minimist(process.argv.slice(2));
