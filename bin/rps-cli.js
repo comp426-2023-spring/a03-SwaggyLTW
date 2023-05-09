244f29983584cb45ed31e078872326bf722a98a0
SwaggyLTW <tommy22@live.unc.edu>
2023-05-08 23:20:54 -0400

    try/catch

--- bin/rps-cli.js
+++ bin/rps-cli.js
@@ -27,4 +27,23 @@ if(args.r || args.rules){
 }
 
 let shot = args._[0];
+try{
 console.log(JSON.stringify(rps(shot)));
+} catch{
+    console.log(`Usage: node-rps [SHOT]
+    Play Rock Paper Scissors (RPS)
+    
+      -h, --help      display this help message and exit
+      -r, --rules     display the rules and exit
+    
+    Examples:
+      node-rps        Return JSON with single player RPS result.
+                      e.g. {"player":"rock"}
+      node-rps rock   Return JSON with results for RPS played against a simulated opponent.
+                      e.g {"player":"rock","opponent":"scissors","result":"win"
+                      Rules for Rock Paper Scissors:
+
+    - Scissors CUTS Paper
+    - Paper COVERS Rock
+    - Rock CRUSHES Scissors`)
+}
--- bin/rpsls-cli.js
+++ bin/rpsls-cli.js
@@ -30,9 +30,34 @@ if(args.r || args.rules){
     - Lizard EATS Paper
     - Paper DISPROVES Spock
     - Spock VAPORIZES Rock
-    - Rock CRUSHES Scissors`)
+    - Rock CRUSHES Scissors`);
 }
 
 let shot = args._[0];
-
+try{
 console.log(JSON.stringify(rpsls(shot)));
+} catch{
+    console.log(`Usage: node-rpsls [SHOT]
+    Play the Lizard-Spock Expansion of Rock Paper Scissors (RPSLS)!
+    
+      -h, --help        display this help message and exit
+      -r, --rules       display the rules and exit
+    
+    Examples:
+      node-rpsls        Return JSON with single player RPSLS result.
+                        e.g. {"player":"rock"}
+      node-rpsls rock   Return JSON with results for RPSLS played against a simulated opponent.
+                        e.g {"player":"rock","opponent":"Spock","result":"lose"}
+                        Rules for the Lizard-Spock Expansion of Rock Paper Scissors:
+
+                        - Scissors CUTS Paper
+                        - Paper COVERS Rock
+                        - Rock SMOOSHES Lizard
+                        - Lizard POISONS Spock
+                        - Spock SMASHES Scissors
+                        - Scissors DECAPITATES Lizard
+                        - Lizard EATS Paper
+                        - Paper DISPROVES Spock
+                        - Spock VAPORIZES Rock
+                        - Rock CRUSHES Scissors`);
+}
